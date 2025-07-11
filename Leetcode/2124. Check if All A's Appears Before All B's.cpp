1cd557cf075d61fde077dad4f5a0f6e6c38d7cf1
Khushali Begde <134138053+khushalibegde@users.noreply.github.com>
2025-07-11 21:24:56 +0530

    solve : 2124. Check if All A's Appears Before All B's.cpp
    level easy

---
+++ Leetcode/2124. Check if All A's Appears Before All B's.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkString(string s) {
+        int flag = 0;
+        for(int i=0;i<s.length();i++){
+            if(s[i] == 'b'){
+                flag = 1;
+            }
+            if(flag == 1 && s[i] == 'a'){
+                return false;
+            }
+        }
+        return true;
+    }
+};
