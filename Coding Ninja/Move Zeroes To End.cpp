458ece24c98cddd229bcd98130a68eefe7da31ec
Khushali Begde <134138053+khushalibegde@users.noreply.github.com>
2025-07-15 23:41:48 +0530

    solve : Move Zeroes To End.cpp
    level moderate

---
+++ Coding Ninja/Move Zeroes To End.cpp
@@ -0,0 +1,16 @@
+void pushZerosAtEnd(vector<int> &nums) 
+{
+	int idx = -1;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]==0){
+            idx=i;
+            break;
+        }
+    }
+    if(idx==-1){return;}
+    for(int i=idx+1;i<nums.size();i++){
+        if(nums[i]!=0){
+            swap(nums[i],nums[idx++]);
+        }
+    }
+}
