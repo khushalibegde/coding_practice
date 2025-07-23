54d67517bdaa9f63bca353cde148a0a9e330ad85
Khushali Begde <134138053+khushalibegde@users.noreply.github.com>
2025-07-23 11:09:38 +0530

    solve : 64. Minimum Path Sum.cpp
    level moderate

---
+++ Leetcode/64. Minimum Path Sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int calc(int i,int j,vector<vector<int>>& grid, vector<vector<int>>& dp){
+        if(i<0 || j<0){return INT_MAX;}
+        if(i==0 && j==0){return grid[i][j];}
+        if(dp[i][j]!=-1){return dp[i][j];}
+
+        int left = calc(i-1,j,grid,dp);
+        int right = calc(i,j-1,grid,dp);
+
+        return dp[i][j] = grid[i][j] + min(left,right);
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+
+        return calc(n-1,m-1,grid,dp);
+    }
+};
