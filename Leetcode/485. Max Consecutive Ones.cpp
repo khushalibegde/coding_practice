9cd27269d27edf3ded759adb951c06c30b05ca61
Khushali Begde <134138053+khushalibegde@users.noreply.github.com>
2025-07-26 19:40:08 +0530

    solve : 485. Max Consecutive Ones.cpp
    level easy

---
+++ Leetcode/485. Max Consecutive Ones.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int maxCnt = 0;
+        int curr = 0;
+        for(int i : nums){
+            if(i==1){
+                curr++;
+                maxCnt = max(maxCnt,curr);
+            }
+            else{
+                curr=0;
+            }
+        }
+        return maxCnt;
+    }
+};
