25dcfc0d29fa27212ab4b958ae7bb2fa4b756eda
Khushali Begde <134138053+khushalibegde@users.noreply.github.com>
2025-07-29 21:51:37 +0530

    solve : Sort An Array of 0s, 1s and 2s.cpp
    level easy

---
+++ Coding Ninja/Sort An Array of 0s, 1s and 2s.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+void sortArray(vector<int>& nums, int n)
+{
+    int left = 0;
+    int right = n-1;
+    int mid = 0;
+    while(mid<=right){
+        if(nums[mid]==0){
+            swap(nums[left++],nums[mid++]);
+        }
+        else if(nums[mid]==1){
+            mid++;
+        }
+        else{
+            swap(nums[right--],nums[mid]);
+        }
+    }
+}
