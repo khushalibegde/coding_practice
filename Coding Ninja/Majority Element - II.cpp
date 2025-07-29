0dfbe2436305b24968a40d5582af413e7bfc7d04
Khushali Begde <134138053+khushalibegde@users.noreply.github.com>
2025-07-29 23:02:08 +0530

    solve : Majority Element - II.cpp
    level easy

---
+++ Coding Ninja/Majority Element - II.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+vector<int> majorityElementII(vector<int> &nums)
+{
+    unordered_map<int, int> freq;
+    vector<int> arr;
+    int n = nums.size(); 
+
+    for (int num : nums) {
+        freq[num]++;
+    }
+    for (auto &p : freq) {
+        if (p.second > n / 3)
+            arr.push_back(p.first);
+    }
+
+    return arr;
+}
